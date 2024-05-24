e1789ed97985fc0e2e683eb6b409615250b6c979
정윤수 (Jeong YunSoo) <47917032+yunut@users.noreply.github.com>
2024-05-24 19:07:57 +0900

    init project

---
+++ src/main/kotlin/com/catches/securities_api/SecuritiesApiApplication.kt
@@ -0,0 +1,11 @@
+package com.catches.securities_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SecuritiesApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<SecuritiesApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/catches/securities_api/SecuritiesApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.catches.securities_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SecuritiesApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
