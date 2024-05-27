01f4363a304c74f23f31eeddbe2e498d1bd4c895
정윤수 (Jeong YunSoo) <47917032+yunut@users.noreply.github.com>
2024-05-27 19:40:05 +0900

    헥사고날 아키텍쳐 초기 구조 작성

---
+++ src/main/kotlin/com/catches/securities_api/adapter/in/web/response/UserResponse.kt
@@ -0,0 +1,6 @@
+package com.catches.securities_api.adapter.`in`.web.response
+
+data class UserResponse(
+    // TODO
+    val id: String,
+)
---
+++ src/main/kotlin/com/catches/securities_api/application/port/UserService.kt
@@ -0,0 +1,7 @@
+package com.catches.securities_api.application.port
+
+import org.springframework.stereotype.Service
+
+@Service
+class UserService {
+}
