4ef9a13ab5955cd9914d74ad7fd5f338c049e54f
정윤수 (Jeong YunSoo) <47917032+yunut@users.noreply.github.com>
2024-05-31 16:40:57 +0900

    스키마 작성

--- src/test/resources/db/init/init.sql
+++ src/test/resources/db/init/init.sql
@@ -1,1 +1,23 @@
 USE securities_info
+
+CREATE TABLE USER(
+     id CHAR(36) PRIMARY KEY,
+     chat_id BIGINT NOT NULL,
+     username VARCHAR(255),
+     status VARCHAR(50),
+     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+     last_active TIMESTAMP,
+     UNIQUE(chat_id)
+);
+
+CREATE TABLE user_bond_history(
+      id CHAR(36) PRIMARY KEY,
+      user_id CHAR(36) NOT NULL,
+      bond_name VARCHAR(255) NOT NULL,
+      bond_rate DECIMAL(5,2) NOT NULL,
+      bond_amount DECIMAL(10,2),
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+      FOREIGN KEY(user_id) REFERENCES USER(id)
+);
